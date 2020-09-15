c4c7df4a38738ce6f012390882d2ffacc288e131
anthonymjimenez <anthonyjim96@gmail.com>
2020-09-15 19:23:47 -0400

    submit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(student, grade)
+        if(!@roster[grade])
+            @roster[grade] = [student]
+        else
+            @roster[grade].push(student)
+        end
+    end
+   
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each { |key, value| 
+        @roster[key]  = value.sort
+    } 
+
+    end
+
+end
